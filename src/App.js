476fea310e6f28c64d4f8d5352466be0c015154a
Yervand.Marikyan <yervand.marikyan@gmail.com>
2023-10-24 18:31:31 +0400

    Creating React Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React from "react";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
-
 //Import Pages
 import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+//Router
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <GlobalStyle/>
-      <AboutUs />
+      <GlobalStyle />
+      <Nav />      
+      <Routes>
+        <Route path="/" exact element={<AboutUs />} />
+        <Route path="/work" exact element={<OurWork />} />
+        <Route path="/contact" element={<ContactUs />} />
+      </Routes>     
     </div>
   );
 }
-
 export default App;
