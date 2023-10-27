49bb3438c9876375e07f29e2d46ae7a605633b4b
Yervand.Marikyan <yervand.marikyan@gmail.com>
2023-10-27 22:13:16 +0400

    Adding animations using framer-motion

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
 import MovieDetail from "./pages/MovieDetail";
 //Router
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+//Animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <GlobalStyle />
-      <Nav />      
-      <Routes>
-        <Route path="/" exact element={<AboutUs />} />
-        <Route path="/work" exact element={<OurWork />} />
-        <Route path="/work/:id" element={<MovieDetail />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>     
+      <Nav />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" exact element={<AboutUs />} />
+          <Route path="/work" exact element={<OurWork />} />
+          <Route path="/work/:id" element={<MovieDetail />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </AnimatePresence> 
     </div>
   );
 }
