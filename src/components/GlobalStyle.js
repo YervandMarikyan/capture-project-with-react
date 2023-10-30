1bd0a8b4a3a9517e95b452bb94ed82bf7b7c94c2
Yervand.Marikyan <yervand.marikyan@gmail.com>
2023-10-30 19:37:40 +0400

    Making mobile friendly

--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -8,10 +8,17 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box
 }
 
+html{
+    @media (max-width: 1700px){
+        font-size: 75%;
+    }    
+}
+
 body{
     background: #1b1b1b;
     font-family: 'Inter', sans-serif;
 }
+
 button{
     font-weight: bold;
     font-size: 1.1.rem;
@@ -27,24 +34,30 @@ button{
         color: white;
     }
 }
+
 h2{
     font-weight: lighter;
     font-size: 4rem;
 }
+
 h3{
     color: white;
 }
+
 h4{
     font-weight: bold;
     font-size: 2rem;       
 }
+
 a{
     font-size: 1.1rem;
 }
+
 span{
     font-weight: bold;
     color:#23d997;
 }
+
 p{
     padding: 3rem 0rem;
     color: #ccc;
--- src/components/useScroll.js
+++ src/components/useScroll.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 
 export const useScroll = () => {
   const controls = useAnimationControls ();
-  const [element, view] = useInView({ threshold: 0.5 });
+  const [element, view] = useInView({ threshold: 0.3 });
   useEffect(() => {
     if (view) {
       controls.start("show");
@@ -12,11 +12,5 @@ export const useScroll = () => {
       controls.start("hidden");
     }
   }, [view]);
-  /* if (view) {
-    controls.start("show");
-  } else {
-    controls.start("hidden");
-  } */
-  //view ? controls.start("show") : controls.start("hidden");
   return [element, controls];
 };
